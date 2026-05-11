2b04e43bb2b8d05044da51969806173f53bb6f20
1freelipe <feliperodrigues_spfc@hotmail.com>
2026-05-11 16:05:35 -0300

    method with from parameters with controller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,11 +7,14 @@ use Illuminate\View\View;
 
 class MainController extends Controller {
     public function showView(): View {
-        $data = [
-            'name' => 'Felipe Rodrigues',
-            'DDD' => '19'
-        ];
+        // $data = [
+        //     'name' => 'Felipe Rodrigues',
+        //     'DDD' => '19'
+        // ];
 
-        return view('home', $data);
+        // return view('home', $data);
+
+        // Passing datas from method with
+        return view('home')->with('name', 'Felipe Rodrigues Teixeira')->with('DDD', '19');
     }
 }
